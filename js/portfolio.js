f40de9a65821510e20a65e1b30b16fc938dc5930
legendscm <scherwainm@gmail.com>
2016-04-24 16:47:42 -0400

    add files to portfolio

---
+++ js/portfolio.js
@@ -0,0 +1,7 @@
+/*This is a temporary code to alert users to stay tune for future projects as the website is still being constructed*/
+$(document).ready(function() {
+  $('.comingsoon').on('click', function(e) {
+    e.preventDefault();
+    alert("Coming Soon!");
+  });//end click
+});//end ready
